36f02668e229c665d4e7434499c614e20fceefd2
Sam Leonard <samuelleonard35@gmail.com>
2023-10-21 20:49:34 +0100

    ✨ Session 3 task two.

--- bft/bft_interp/src/lib.rs
+++ bft/bft_interp/src/lib.rs
@@ -2,3 +2,6 @@
 #![deny(clippy::missing_docs_in_private_items)]
 
 //! the brainfuck interpreter
+
+mod machine;
+pub use machine::Machine;
---
+++ bft/bft_interp/src/machine.rs
@@ -0,0 +1,54 @@
+//! The brainfuck virtual machine
+
+use std::num::NonZeroUsize;
+
+/// The brainfuck virtual machine state
+#[derive(Debug, Clone)]
+pub struct Machine {
+    /// The memory backing the virtual machine
+    tape: Vec<u8>,
+
+    /// Can the tape grow?
+    tape_can_grow: bool,
+}
+
+/// The default size of the virtual machine's tape
+pub const DEFAULT_TAPE_SIZE: usize = 30_000;
+
+impl Default for Machine {
+    fn default() -> Self {
+        Self::new(DEFAULT_TAPE_SIZE)
+    }
+}
+
+impl Machine {
+    /// Create a new virtual machine with a growable tape
+    ///
+    /// `tape_size`: the size of the tape to allocate for the virtual machine
+    ///
+    /// ```
+    /// # use bft_interp::Machine;
+    /// let vm = Machine::new(1000);
+    /// ```
+    pub fn new(tape_size: usize) -> Self {
+        Self {
+            tape: vec![0; tape_size],
+            tape_can_grow: true,
+        }
+    }
+
+    /// Create a new virtual machine with a fixed size tape
+    ///
+    /// `tape_size`: the size of the tape to allocate for the virtual machine
+    ///
+    /// ```
+    /// # use bft_interp::Machine;
+    /// let vm = Machine::new_fixed_size(1000);
+    /// ```
+    pub fn new_fixed_size(tape_size: usize) -> Self {
+        Self {
+            tape: vec![0; tape_size],
+            tape_can_grow: false,
+        }
+    }
+}
