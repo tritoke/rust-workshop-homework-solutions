c8341874b078bebd55486612b0cc45b907c55863
Sam Leonard <samuelleonard35@gmail.com>
2023-10-22 14:57:57 +0100

    ✨ Session 5 task three.

--- bft/bft_interp/src/machine.rs
+++ bft/bft_interp/src/machine.rs
@@ -1,5 +1,7 @@
 //! The brainfuck virtual machine
 
+use std::io::{self, Read, Write};
+
 use bft_types::Program;
 
 /// The brainfuck virtual machine state
@@ -41,17 +43,47 @@ pub trait CellKind: Default + Clone {
 
     /// Decrement the cell by one, wrapping the result of the computation
     fn wrapping_dec(&mut self);
+
+    /// Set the value of the cell
+    fn set_value(&mut self, value: u8);
+
+    /// The value of the cell as a slice of bytes
+    fn as_bytes(&self) -> Box<[u8]>;
 }
 
-impl CellKind for u8 {
-    fn wrapping_inc(&mut self) {
-        *self = self.wrapping_add(1);
-    }
+/// Implement CellKind for a builtin numeric type
+macro_rules! cell_kind_impl {
+    ($type:ty) => {
+        impl CellKind for $type {
+            fn wrapping_inc(&mut self) {
+                *self = self.wrapping_add(1);
+            }
 
-    fn wrapping_dec(&mut self) {
-        *self = self.wrapping_sub(1);
-    }
+            fn wrapping_dec(&mut self) {
+                *self = self.wrapping_sub(1);
+            }
+
+            fn set_value(&mut self, value: u8) {
+                *self = value.into();
+            }
+
+            fn as_bytes(&self) -> Box<[u8]> {
+                Box::new(self.to_be_bytes())
+            }
+        }
+    };
 }
+
+/// Implement CellKind for a list of builtin types
+macro_rules! cell_kind_impl_all {
+    ($($type:ty),+) => {
+        $(
+            cell_kind_impl!($type);
+        )*
+    };
+}
+
+cell_kind_impl_all!(u8, u16, u32, u64, u128, i16, i32, i64, i128);
 
 #[allow(dead_code)]
 impl<'a, Cell: CellKind> Machine<'a, Cell> {
@@ -119,6 +151,9 @@ impl<'a, Cell: CellKind> Machine<'a, Cell> {
                 self.tape.reserve(1);
                 self.tape.resize(self.tape.capacity(), Cell::default());
             } else {
+                // move head left doesn't affect the dp on error
+                // we should behave the same
+                self.dp -= 1;
                 return Err(InterpretError::TapeRunOffError {
                     ip_at_error: self.ip,
                 });
@@ -137,15 +172,52 @@ impl<'a, Cell: CellKind> Machine<'a, Cell> {
     fn decrement_cell(&mut self) {
         self.tape[self.dp].wrapping_dec()
     }
+
+    /// Read a single u8 from a reader and assign it to the value of the tape
+    fn read_value(&mut self, reader: &mut impl Read) -> Result<(), InterpretError> {
+        let mut buf = [0u8];
+        if let Err(inner) = reader.read_exact(&mut buf) {
+            return Err(InterpretError::IoError {
+                ip_at_error: self.ip,
+                inner,
+            });
+        };
+
+        self.tape[self.dp].set_value(buf[0]);
+
+        Ok(())
+    }
+
+    /// Write the big-endian byte value of the current cell into the writer
+    fn write_value(&mut self, writer: &mut impl Write) -> Result<(), InterpretError> {
+        let buf = self.tape[self.dp].as_bytes();
+
+        if let Err(inner) = writer.write_all(&buf) {
+            return Err(InterpretError::IoError {
+                ip_at_error: self.ip,
+                inner,
+            });
+        };
+
+        Ok(())
+    }
 }
 
 /// errors that can occor while interpreting a brainfuck program
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug)]
 pub enum InterpretError {
     /// The data pointer exceed the bounds of the tape
     TapeRunOffError {
         /// The instruction which lead to the error
         ip_at_error: usize,
+    },
+
+    /// The virtual machine failed to perform an IO operation
+    IoError {
+        /// The instruction which lead to the error
+        ip_at_error: usize,
+        /// The inner IO error which caused the failure
+        inner: io::Error,
     },
 }
 
@@ -175,10 +247,10 @@ mod tests {
         for _ in 0..99 {
             machine.move_head_right().unwrap();
         }
-        assert_eq!(
+        assert!(matches!(
             machine.move_head_right().unwrap_err(),
             InterpretError::TapeRunOffError { ip_at_error: 0 }
-        );
+        ));
     }
 
     #[test]
@@ -201,13 +273,10 @@ mod tests {
         let prog = Program::from_file("../programs/example.bf").unwrap();
         let mut machine = Machine::<u8>::new(100, TapeKind::FixedSize, &prog);
 
-        for _ in 0..99 {
-            machine.move_head_right().unwrap();
-        }
-        assert_eq!(
-            machine.move_head_right().unwrap_err(),
+        assert!(matches!(
+            machine.move_head_left().unwrap_err(),
             InterpretError::TapeRunOffError { ip_at_error: 0 }
-        );
+        ));
     }
 
     #[test]
@@ -218,7 +287,7 @@ mod tests {
         machine.increment_cell();
         assert_eq!(machine.tape[0], 1);
 
-        machine.tape[0] = 0xFF;
+        machine.tape[0].set_value(0xFF);
         machine.increment_cell();
         assert_eq!(machine.tape[0], 0);
     }
@@ -232,5 +301,48 @@ mod tests {
         assert_eq!(machine.tape[0], 0xFF);
         machine.decrement_cell();
         assert_eq!(machine.tape[0], 0xFE);
+    }
+
+    #[test]
+    fn test_read_value() {
+        let prog = Program::from_file("../programs/example.bf").unwrap();
+        let mut machine = Machine::<u8>::new(100, TapeKind::FixedSize, &prog);
+
+        let data = 0xDEADBEEF_u32.to_be_bytes();
+        let mut reader = std::io::Cursor::new(data);
+        for d in data {
+            machine.read_value(&mut reader).unwrap();
+            assert_eq!(machine.tape[0], d);
+        }
+        machine.read_value(&mut reader).unwrap_err();
+    }
+
+    #[test]
+    fn test_write_value() {
+        let prog = Program::from_file("../programs/example.bf").unwrap();
+        let mut machine = Machine::<u8>::new(100, TapeKind::FixedSize, &prog);
+
+        for i in u8::MIN..=u8::MAX {
+            let mut writer = std::io::Cursor::new(vec![0u8]);
+            machine.tape[0].set_value(i);
+            machine.write_value(&mut writer).unwrap();
+            assert_eq!(writer.get_ref(), &[i]);
+        }
+    }
+
+    #[test]
+    fn test_write_value_is_be() {
+        let prog = Program::from_file("../programs/example.bf").unwrap();
+        let mut machine = Machine::<u32>::new(100, TapeKind::FixedSize, &prog);
+
+        let val = 0xDEAD_BEEF;
+        machine.tape[0] = val;
+        let mut writer = std::io::Cursor::new(vec![0u8; 4]);
+        machine.write_value(&mut writer).unwrap();
+        machine.write_value(&mut writer).unwrap();
+        machine.write_value(&mut writer).unwrap();
+        machine.write_value(&mut writer).unwrap();
+
+        assert_eq!(&writer.get_ref()[..4], &val.to_be_bytes());
     }
 }
